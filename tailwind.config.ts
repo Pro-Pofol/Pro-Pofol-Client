96c0ab4c3640a809153cf7c88db5fe0269f4a0de
eternrust <eternrust@dsm.hs.kr>
2024-03-31 22:03:10 +0900

    ADD :: color & typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,126 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      blue50: "#EBF0FE",
+      blue100: "#D5DFFB",
+      blue200: "#ABBFF8",
+      blue300: "#ABBFF8",
+      blue400: "#5780F1",
+      blue500: "#2D60ED",
+      blue600: "#244DBE",
+      blue700: "#1B3A8E",
+      blue800: "#12265F",
+      blue900: "#0C193E",
+      blue950: "#09132F",
+      gray50: "#FAFAFA",
+      gray100: "#F2F3F5",
+      gray200: "#E6E7EB",
+      gray300: "#C6C8CC",
+      gray400: "#ACAEB3",
+      gray500: "#919399",
+      gray600: "#787A80",
+      gray700: "#5F6166",
+      gray800: "#46484D",
+      gray900: "#2E3033",
+      gray950: "#17181A",
+      attention: '#3349D8',
+      success: '#2C8C1C',
+      coution: '#B75F00',
+      critical: '#DB2C36',
+      attentionBackground: '#EFF4F4',
+      successBackground: '#E7FAD4',
+      coutionBackground: '#FFF2CC',
+      criticalBackground: '#FFE6D8',
+      transparent: 'transparent',
+      current: 'currentColor'
     },
+    fontSize: {
+      headlineLarge: [
+        '64px',
+        {
+          fontWeight: 700,
+          lineHeight: '72px',
+        },
+      ],
+      headlineMedium: [
+        '648x',
+        {
+          fontWeight: 700,
+          lineHeight: '52px',
+        },
+      ],
+      headlineSmall: [
+        '40px',
+        {
+          fontWeight: 700,
+          lineHeight: '42px',
+        },
+      ],
+      titleLarge: [
+        '32px',
+        {
+          fontWeight: 600,
+          lineHeight: '48px',
+        },
+      ],
+      titleMedium: [
+        '24px',
+        {
+          fontWeight: 600,
+          lineHeight: '36px',
+        },
+      ],
+      titleSmall: [
+        '20px',
+        {
+          fontWeight: 600,
+          lineHeight: '32px',
+        },
+      ],
+      bodyLarge: [
+        '20px',
+        {
+          fontWeight: 500,
+          lineHeight: '28px',
+        },
+      ],
+      bodyMedium: [
+        '18px',
+        {
+          fontWeight: 400,
+          lineHeight: '26px',
+        },
+      ],
+      bodySmall: [
+        '16px',
+        {
+          fontWeight: 400,
+          lineHeight: '24px',
+        },
+      ],
+      labelLarge: [
+        '14px',
+        {
+          fontWeight: 500,
+          lineHeight: '20px',
+        },
+      ],
+      labelMedium: [
+        '14px',
+        {
+          fontWeight: 400,
+          lineHeight: '20px',
+        },
+      ],
+      labelSmall: [
+        '12px',
+        {
+          fontWeight: 400,
+          lineHeight: '16px',
+        },
+      ],
+    }
   },
   plugins: [],
 };
