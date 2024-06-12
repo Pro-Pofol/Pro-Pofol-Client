a9f9ba053780a4ef7e422afc1fda0ed379840cde
SeungWoo <wjknn3123@gmail.com>
2024-06-12 11:55:08 +0900

    ADD :: 마이페이지 내가 공유한 지원서 & 팁

--- src/app/profile/application/page.tsx
+++ src/app/profile/application/page.tsx
@@ -1,23 +1,23 @@
-'use client'
+import { ApplicationBox, BackButton } from '@/components'
+import { getMe, getRecommend } from '@/services'
 
-import { Arrow } from '@/assets'
-import { ApplicationBox } from '@/components'
-import { applicationData } from '@/constants'
-import { useRouter } from 'next/navigation'
-
-export default function MyApplicationPage() {
-  const router = useRouter()
+export default async function MyApplicationPage() {
+  const { oauth_id } = await getMe()
+  const applicationData = await getRecommend().then((value) =>
+    value.data.posts
+      .filter(({ post_writer_id }) => oauth_id === post_writer_id)
+      .sort(
+        (a, b) =>
+          new Date(b.post_created_at).getTime() -
+          new Date(a.post_created_at).getTime(),
+      ),
+  )
 
   return (
     <main className="min-h-[100dvh] p-[60px_0_120px] flex justify-center">
       <section className="flex flex-col gap-10 px-10 sm:px-6 max-w-[1280px] w-full">
         <div className="flex flex-col gap-8">
-          <button
-            onClick={() => router.back()}
-            className="p-2 rounded-lg border border-gray200 group hover:bg-gray50 transition-colors w-fit"
-          >
-            <Arrow className="group-hover:-translate-x-1 transition-transform" />
-          </button>
+          <BackButton />
           <div className="flex flex-col gap-2">
             <h3 className="text-titleLarge">공유한 지원서 자료</h3>
             <p className="text-bodySmall text-gray600">8개 내 지원서 자료</p>
--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Arrow } from '@/assets'
 import { useRouter } from 'next/navigation'
 
